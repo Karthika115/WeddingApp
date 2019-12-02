5a11be22103089476479b371dd33d7af6346a134
Karthika115 <52747237+Karthika115@users.noreply.github.com>
2019-12-02 11:46:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+import Splash from './components/Splash';
+import Login from './components/Login';
+AppRegistry.registerComponent('HappyWedding', () => Login);
